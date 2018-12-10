ccdd1ce8989f7cbbcf98c23f36465b7e9294472f
Richard Yu <37787489+f22604026@users.noreply.github.com>
2018-12-10 10:35:18 -0500

    ir signal

--- motor_control.py
+++ motor_control.py
@@ -5,12 +5,19 @@ import time
 
 wpi.wiringPiSetup()
 wpi.pinMode(5, 1)
+wpi.pinMode(23, 1)
 
 while True:
-    input = raw_input("Secure the package, enter '1' \nRelease the package, enter '2':\n ==> ")
+    input = raw_input("Secure the package, enter '1' \nRelease the package, enter '2'  \ndoor opening, enter '3':\n ==> ")
     if input == "1" :
         print("Package is secured! \n")
         wpi.digitalWrite(5, 0)
     if input == "2" :
         print("Package is released! \n")
         wpi.digitalWrite(5, 1)
+
+    if input == "3" :
+        print("door opening! \n")
+        wpi.digitalWrite(23, 1)
+        time.sleep(1000)
+        wpi.digitalWrite(23, 0)
